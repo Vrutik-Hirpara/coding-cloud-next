ef484ed904969d0951e90662641a939471d646f1
Vrutik Hirpara <vrutikhirpara01@gmail.com>
2026-02-21 12:01:40 +0530

    changes in file and add files

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,36 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: [
+//       "codingcloud.pythonanywhere.com", // 🔥 your backend media
+//       "i.pravatar.cc",                  // 🔥 avatar image domain
+//     ],
+//   },
+
+//   reactStrictMode: true,
+
+
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: [
-      "codingcloud.pythonanywhere.com", // 🔥 your backend media
-      "i.pravatar.cc",                  // 🔥 avatar image domain
-    ],
-  },
-
   reactStrictMode: true,
 
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "codingcloud.pythonanywhere.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
