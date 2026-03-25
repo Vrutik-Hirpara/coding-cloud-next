f8c70745fecb4a86ee09f0994f24003e8015512b
Vrutik Hirpara <vrutikhirpara01@gmail.com>
2026-03-25 17:29:22 +0530

    changes in api.ts file

--- lib/api.ts
+++ lib/api.ts
@@ -1,66 +1,3 @@
-// // src/lib/api.ts
-
-// // 🔥 Base URL (backend)
-// export const BASE_URL = "https://codingcloud.pythonanywhere.com";
-
-// // 🔥 All API endpoints
-// export const API = {
-//   // CATEGORY: {
-//   //   LIST: `${BASE_URL}/category/`,
-//   //   DETAIL: (id: number | string) => `${BASE_URL}/category/${id}/`,
-
-//   //       TOTALS: `${BASE_URL}/category_totals/`,
-
-//   // },
-//     CATEGORY: {
-//     LIST: `${BASE_URL}/category/`,
-//     DETAIL: (id: number | string) => `${BASE_URL}/category/${id}/`,
-//     DETAIL_BY_SLUG: (slug: string) => `${BASE_URL}/course/category/${slug}/`, // 🔥 NEW: Direct slug-based endpoint
-//     TOTALS: `${BASE_URL}/category_totals/`,
-//   },
-
-//   COURSES: {
-//     LIST: `${BASE_URL}/course/`,
-//     DETAIL: (id: number | string) => `${BASE_URL}/course/${id}/`,
-//   },
-
-//   FAQS: {
-//     LIST: `${BASE_URL}/faqs/`,
-//     DETAIL: (id: number | string) => `${BASE_URL}/faqs/${id}/`,
-//   },
-
-//   MODULES: {
-//     LIST: `${BASE_URL}/modules/`,
-//     DETAIL: (id: number | string) => `${BASE_URL}/modules/${id}/`,
-//   },
-
-//   TOPICS: {
-//     LIST: `${BASE_URL}/topics/`,
-//     DETAIL: (id: number | string) => `${BASE_URL}/topics/${id}/`,
-//   },
-
-//   BANNERS: {
-//     LIST: `${BASE_URL}/banners/`,
-//     DETAIL: (id: number | string) => `${BASE_URL}/banners/${id}/`,
-//   },
-
-//   TESTIMONIALS: {
-//     LIST: `${BASE_URL}/testimonials/`,
-//     DETAIL: (id: number | string) => `${BASE_URL}/testimonials/${id}/`,
-//   },
-
-//   REGISTER_MSG: {
-//     LIST: `${BASE_URL}/register_msg/`,
-//     DELETE: (id: number | string) =>
-//       `${BASE_URL}/register_msg/${id}/`,
-//   },
-
-//   BLOGS: {
-//     LIST: `${BASE_URL}/blogs/`,
-//     DETAIL: (slug: string) => `${BASE_URL}/blogs/${slug}/`,
-//   },
-// };
-
 
 
 // // src/lib/api.ts
@@ -312,8 +249,8 @@
 // src/lib/api.ts
 
 // 🔥 Base URL (backend)
-// export const BASE_URL = "https://codingcloud.pythonanywhere.com";
-export const BASE_URL = "http://codingcloudapi.codingcloud.co.in";
+export const BASE_URL = "https://codingcloud.pythonanywhere.com";
+// export const BASE_URL = "http://codingcloudapi.codingcloud.co.in";
 
 // 🔥 All API endpoints
 export const API = {
