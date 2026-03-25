a45edc754f8e446c988fa7d386bb43de88a0675a
Vrutik Hirpara <vrutikhirpara01@gmail.com>
2026-03-25 17:18:50 +0530

    changes in file and add filesv1

--- lib/api.ts
+++ lib/api.ts
@@ -312,8 +312,8 @@
 // src/lib/api.ts
 
 // 🔥 Base URL (backend)
-export const BASE_URL = "https://codingcloud.pythonanywhere.com";
-// export const BASE_URL = "http://codingcloudapi.codingcloud.co.in";
+// export const BASE_URL = "https://codingcloud.pythonanywhere.com";
+export const BASE_URL = "http://codingcloudapi.codingcloud.co.in";
 
 // 🔥 All API endpoints
 export const API = {
